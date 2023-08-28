201e7699b2832316c9905cd80535f5320c7bc7f1
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-08-28 15:44:29 +0530

    modification is merger func

--- rt-assessment-answer/mergedSorted.js
+++ rt-assessment-answer/mergedSorted.js
@@ -5,8 +5,26 @@ let arr1 = [1, 3, 5];
 let arr2 = [2, 4];
 
 const mergeSorted = (arr1, arr2) => {
-  let result = [...arr1, ...arr2].sort((a, b) => b - a);
-  return result;
+  let i = 0;
+  let j = 0;
+  let result = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result.reverse();
 };
-
 console.log(mergeSorted(arr1, arr2));
