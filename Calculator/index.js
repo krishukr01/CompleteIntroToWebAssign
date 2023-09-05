3c352c5496511cee429beb121d4b148b120f00a2
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-09-05 11:40:01 +0530

    Calculator Assign : html css added

---
+++ Calculator/index.js
@@ -0,0 +1,5 @@
+// Event : handlers
+var clickMeButton = document.querySelector(".button-group");
+clickMeButton.addEventListener("click", function (event) {
+  console.log("JS file is working well");
+});
