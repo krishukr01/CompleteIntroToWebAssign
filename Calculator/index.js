e55026f83e35ab25545c86579e43c5005fe0ba1f
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-09-05 12:47:58 +0530

    Adding javascript

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,5 +1,39 @@
-// Event : handlers
-var clickMeButton = document.querySelector(".button-group");
-clickMeButton.addEventListener("click", function (event) {
-  console.log("JS file is working well");
-});
+let screenValue = "0";
+let screen = document.querySelector(".screen");
+// Event handlers : for handle number click
+const handleNumber = (value) => {
+  if (screenValue === "0") {
+    screenValue = value;
+  } else {
+    screenValue += value;
+  }
+  reRender();
+};
+
+// Event Handler : For handle symbol click
+const handleSymbol = (value) => {
+  console.log(screenValue);
+  console.log("Symbol is clicked");
+};
+
+// Event handlers : For Handle Click
+const handleClick = (value) => {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+};
+
+// Re-render after change happens
+const reRender = () => {
+  screen.innerHTML = screenValue;
+};
+
+const init = () => {
+  document.querySelector(".calc-buttons").addEventListener("click", (event) => {
+    handleClick(event.target.innerHTML);
+  });
+};
+
+init();
